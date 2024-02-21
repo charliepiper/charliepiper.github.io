d3105b2849bdcdae8b04f9a2ac6583d7035a800c
charliepiper <charliepiperr@gmail.com>
2024-02-21 16:20:47 +0000

    fix &apos;

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
             <div className="md:pt-10 pt-1">
                 <div className="max-w-xl md:max-w-3xl">
                     <p className="text-xl md:text-2xl">hello!</p>
-                    <h2 className="font-semibold text-4xl  md:text-5xl ">We're <span className=" font-bold">/cohort1/group1</span></h2>
+                    <h2 className="font-semibold text-4xl  md:text-5xl ">We&apos;re <span className=" font-bold">/cohort1/group1</span></h2>
                     <p className = "text-sm md:text-base mt-4 mb-6">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi commodo tellus sit amet magna tempor vestibulum. Sed 
                         <span className=" font-bold"> lobortis laoreet sem,</span> ut laoreet erat aliquam non. 
