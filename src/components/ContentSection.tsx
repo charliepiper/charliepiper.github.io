fa743e26a79a6eaabc32986ab646573d01aedbf2
charliepiper <69611203+charliepiper@users.noreply.github.com>
2024-03-21 10:17:17 +0000

    feat: added method selection

--- src/components/ContentSection.tsx
+++ src/components/ContentSection.tsx
@@ -43,7 +43,7 @@ const HeroSection = () => {
                     <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
                     <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 ">Method selection and planning</h5>
                     <p className="font-normal text-gray-700 pb-5">Here is a link to our method selection and planning pdf</p>
-                    <a className="cursor-pointer">
+                    <a className="cursor-pointer" href="methodselection.pdf" target="_blank" rel="noopener noreferrer" download>
                         <h6 className="text-sm ghosted text-blue-500">/methodselectionandplanning.pdf</h6>
                     </a>
                     </div>
